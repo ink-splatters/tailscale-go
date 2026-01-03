2e667fe30c40444ee6977804b73d6f603d5ad2c4
Peter A. <ink.splatters@pm.me>
2026-01-03 04:48:10 +0100

    nix flake
    Signed-off-by: Peter A. <ink.splatters@pm.me>

---
+++ nix/dev/default.nix
@@ -0,0 +1,10 @@
+{inputs, ...}: {
+  imports = [
+    inputs.git-hooks.flakeModule
+    ./pre-commit.nix
+    ./shell.nix
+  ];
+  perSystem = {pkgs, ...}: {
+    formatter = pkgs.alejandra;
+  };
+}
