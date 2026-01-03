5960ef6057784b27be35feba76ddcc28aa3b5e29
Peter A. <ink.splatters@pm.me>
2026-01-03 04:48:10 +0100

    nix flake
    Signed-off-by: Peter A. <ink.splatters@pm.me>

---
+++ nix/dev/shell.nix
@@ -0,0 +1,20 @@
+{
+  perSystem = {
+    config,
+    pkgs,
+    ...
+  }: let
+    inherit (config) pre-commit;
+  in {
+    devShells.default = pkgs.mkShell {
+      packages = pre-commit.settings.enabledPackages ++ [pkgs.gitMinimal];
+      shellHook = ''
+        ${pre-commit.installationScript}
+      '';
+
+      env = {
+        inherit (pkgs.go_1_25) GOROOT_BOOTSTRAP;
+      };
+    };
+  };
+}
