461996cc0258b82225cda5bd53d2f7a4d114022d
Peter A. <ink.splatters@pm.me>
2026-02-22 19:08:07 +0100

    nix/flake-module.nix:
    generate derivation version in semver compatible format from the both
    lines of ./VERSION
    
    Signed-off-by: Peter A. <ink.splatters@pm.me>

--- nix/flake-module.nix
+++ nix/flake-module.nix
@@ -1,5 +1,6 @@
 {lib, ...}: let
   inherit (lib) lists match fileContents splitString;
+  inherit (builtins) elemAt removeAttrs replaceStrings;
 in {
   perSystem = {
     config,
@@ -7,13 +8,15 @@ in {
     ...
   }: let
     inherit (config) src;
-    version = let
-      versionStr = lists.head (splitString "\n" (fileContents "${src}/VERSION"));
-    in
-      lists.head (match "go(.*)" versionStr);
+    name = let
+      versionLines = splitString "\n" (fileContents "${src}/VERSION");
+      versionStr = lists.head versionLines;
+      timeIsoExtended = lists.head (match "time (.*)" (elemAt versionLines 1));
+      timeIsoBasic = replaceStrings ["-" ":"] ["" ""] timeIsoExtended;
+    in "${versionStr}+tailscale.${timeIsoBasic}";
   in {
-    packages.go_1_26 = pkgs.go_1_26.overrideAttrs (_: {
-      version = "${version}-tailscale";
+    packages.go_1_26 = (removeAttrs pkgs.go_1_26 ["pname" "version"]).overrideAttrs (_: {
+      inherit name;
       inherit src;
     });
   };
