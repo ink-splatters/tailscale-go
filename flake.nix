53db44531ea27cc11ca58c070f80a2ef4441c73f
Peter A. <ink.splatters@pm.me>
2026-04-02 08:51:25 +0200

    * flake input renamed: default-systems -> nix-systems: * Updated input 'systems':     'github:ink-splatters/default-systems/29e147264c82178afdbd7c83d5429bd0925f8883?narHash=sha256-uuanbkFk8a313Zfrxdcz8Us/yQtaxNnKJtX8v0ZPpd8%3D' (2026-03-07)   → 'github:ink-splatters/nix-systems/29e147264c82178afdbd7c83d5429bd0925f8883?narHash=sha256-uuanbkFk8a313Zfrxdcz8Us/yQtaxNnKJtX8v0ZPpd8%3D' (2026-03-07)

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    systems.url = "github:ink-splatters/default-systems"; # no x86_64-darwin
+    systems.url = "github:ink-splatters/nix-systems"; # no x86_64-darwin
   };
 
   nixConfig = {
